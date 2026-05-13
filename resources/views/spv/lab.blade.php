c9b59594c98ac21ac21dff25492bb2c83af70d14
Sanex77 <danangatmojo916@gmail.com>
2026-05-14 01:00:56 +0700

    Menambahkan fitur tambah jadwal manual dan perbaikan filter controller

---
+++ resources/views/spv/lab.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Lab</title>
+</head>
+<body>
+@extends('layouts.spv')
+@section('title', 'Manajemen Jadwal')
+
+
+@section('content')
+
+@endsection   
+</body>
+</html>
